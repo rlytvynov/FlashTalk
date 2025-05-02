1e2067079e252e890ffc7bf04c9dbbbb419c8557
Michael Zarkov <michael.zarkov@gmail.com>
2025-05-02 23:54:12 +0300

    Server socket authentication.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,8 @@ import cors from "cors"
 
 import { SERVER_HOSTNAME, SERVER_PORT } from "@config/envConfig";
 import corsOptions from "@config/corsConfig";
+import { JWT_SECRET } from '@config/envConfig'
+import jwt from 'jsonwebtoken'
 import responseMiddleware from "@middlewares/responseTypeMiddlware";
 import authRouter from "@routes/authRouter";
 import usersRouter from "@routes/usersRouter";
@@ -38,22 +40,36 @@ app.use('/api/channels', channelsRouter);
  * Сокет соединение - DR4
  */
 const server = http.createServer(app);
-const io = new Server(server, {
-    cors: corsOptions
+const io = new Server(server, { cors: corsOptions });
+
+// Middleware to verify the user token.
+io.use((socket, next) => {
+    const token = socket.handshake.headers['authorization'];  // The user must provide a token when initiating a connection.
+    if (!token) return next(new Error('Authorization token is missing!'));
+
+    try {
+        jwt.verify(token, JWT_SECRET) as { id: string };
+    } catch (err) {  // Should do better error handling here.
+        return next(new Error('Invalid token!'));
+    }
 });
-//midlleware за authorizаtion на оптребителя за соета
-const userSessions: Map<string, string> = new Map<string, string>();
+
+// Keep track of connected users.
+const userSessions: Map<string, string> = new Map<string, string>();  // (socketID, userID)
 io.on("connection", (socket) => {
-    console.log("Нов потребител е свързан: ", socket.id);
-    //примерно добаяне на сокет ид на потребител
-    // userSessions.set(socket.id, socket.user.id.toString())
-    // const associatedChannels =  await pool.query("SELECT * FROM user_channels($1)", [socket.user.id])
+    const token = socket.handshake.headers['authorization'] as string;  // Already verified in the io middleware.
+    const user = jwt.verify(token, JWT_SECRET) as { id: string, username: string };  // Get the user ID.
+    console.log(`[server]: ${user.username} connected.`);
+
+    userSessions.set(socket.id, user.id);
+    
+    // const associatedChannels =  await pool.query("SELECT * FROM user_channels($1)", [user.id])
     // for (const channel of associatedChannels.rows) {
     //     socket.join(channel.id)
-    //     socket.broadcast.to(channel.id).emit('user-connected', socket.user.id.toString());
-    //     console.log("Потребителят е добавен в канала:", channel.name);
+    //     socket.broadcast.to(channel.id).emit('user-connected', user.id);
     // }
-    //примерен обработчик на събитие
+    
+    // Example event handler.
     // socket.on("new-message", async (message) => {
     //     const messages = await pool.query("SELECT * FROM create_message($1, $2, $3, $4)", [message.channelid, message.authorid, message.data ])
     //     //примерен broadcast emit
